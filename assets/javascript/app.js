6e7845433a6cde570e29e9036bfc72ec5e954741
Mark Bui <tvyybui@yahoo.com>
2018-01-08 00:44:23 -0800

    added onclick and global enter events to grab input from ui

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,51 @@
+// Budget Controller
 var budgetController = (function() {
 
-    var x = 23;
 
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
 })();
 
+// UI Controller
 var UIController = (function() {
 
+    return {
+        getinput: function() {
+            // returning an object with all the UI
+            return {
+                type: document.querySelector('.add__type').value,
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value,
+            }
+
+        }
+    };
+
 })();
 
+// Main App Controller
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(7);
+    var ctrlAddItem = function() {
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // get filled input data
+        var input = UICtrl.getinput();
+        console.log(input);
+        // add item to the budget controller
+        // add the item to the UI interface
+        // calculate the budget
+        // display budget
+
     }
+
+    // onclick event to happen when a user clicks on the add_btn, calling the crtlAddItem function
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    // global 'return/enter' button press event listener
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keycode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+    });
+
 })(budgetController, UIController);
